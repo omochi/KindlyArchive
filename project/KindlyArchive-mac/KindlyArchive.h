65d150597dcf7f3fa9f0bad234211fb612a1c0d9
omochimetaru <omochi.metaru@gmail.com>
2018-03-16 15:49:50 +0900

    mac proj

---
+++ project/KindlyArchive-mac/KindlyArchive.h
@@ -0,0 +1,19 @@
+//
+//  KindlyArchive.h
+//  KindlyArchive
+//
+//  Created by omochimetaru on 2018/03/16.
+//  Copyright © 2018年 omochimetaru. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KindlyArchive.
+FOUNDATION_EXPORT double KindlyArchiveVersionNumber;
+
+//! Project version string for KindlyArchive.
+FOUNDATION_EXPORT const unsigned char KindlyArchiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KindlyArchive/PublicHeader.h>
+
+
